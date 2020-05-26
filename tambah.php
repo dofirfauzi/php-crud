da8c3e859877ffc9fa4eefba80db978f85da7910
Dofir Fauzi <dofirfauzi@tazkia.ac.id>
2020-05-26 11:23:16 +0700

    menambahkan validasi pada form tambah data

--- tambah.php
+++ tambah.php
@@ -1,17 +1,43 @@
 <?php
 require('functions/functions.php');
-if (isset($_POST["tambah"])) {
-    if (tambah($_POST) > 0) {
-        echo "
-        <script> alert('Terimakasih, data berhasil ditambahkan!');
-        document.location.href='index.php';
-        </script>
-        ";
-    } else {
-        echo "
-        <script> alert('Mohon maaf, data gagal ditambahkan!');
-        </script>
-        ";
+// cek apakah tombol submit sudah di klik
+if (isset($_POST["submit"])) {
+    // buat array untuk menampung error
+    $error = [];
+    // jalankan form validation
+    // jika input name kosong
+    if (empty($_POST["judul"])) {
+        // masukan kedalam array erorr nama
+        $error["judul"] = "Judul tidak boleh kosong!";
+    }
+    // jika input username kosong
+    if (empty($_POST["pengarang"])) {
+        // masukan kedalam array erorr username
+        $error["pengarang"] = "Pengarang tidak boleh kosong!";
+    }
+    if (empty($_POST["penerbit"])) {
+        $error["penerbit"] = "Penerbit tidak boleh kosong!";
+    }
+    if (empty($_POST["cover"])) {
+        $error["cover"] = "Cover tidak boleh kosong!";
+    }
+
+    // jika array error kosong
+    if (count($error) == 0) {
+        // jalankan fungsi register
+        if (tambah($_POST) > 0) {
+            // jika registrasi berhasil tampilkan alert
+            echo "<script>
+            alert('Selamat Data Berhasil ditambahkan');
+            document.location.href='../';
+            </script>";
+        } else {
+            // jika gagal tampilkan error dari koneksi
+            echo "<script>
+            alert('mohon Maaf Data gagal ditambahkan');
+            document.location.href='../';
+            </script>";
+        }
     }
 }
 // header
@@ -27,23 +53,30 @@ require('template/header.php');
     <form action="" method="post">
         <div class="form-group">
             <label for="judul">Judul Buku :</label>
-            <input type="text" class="form-control" id="judul" name="judul">
-            <small id="judul" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <input type="text" class="form-control" id="judul" name="judul" value="<?php if (isset($_POST["submit"])) echo $_POST["judul"]; ?>">
+            <?php if(isset($error["judul"])) {?>
+            <small id="judul" class="form-text text-danger"><?php echo $error["judul"];?></small> <?php } ?>
         </div>
         <div class="form-group">
             <label for="pengarang">Pengarang :</label>
-            <input type="text" class="form-control" id="pengarang" name="pengarang">
+            <input type="text" class="form-control" id="pengarang" name="pengarang" value="<?php if (isset($_POST["submit"])) echo $_POST["pengarang"]; ?>">
+            <?php if(isset($error["pengarang"])) {?>
+            <small id="judul" class="form-text text-danger"><?php echo $error["pengarang"];?></small> <?php } ?>
         </div>
         <div class="form-group">
             <label for="penerbit">Penerbit :</label>
-            <input type="text" class="form-control" id="penerbit" name="penerbit">
+            <input type="text" class="form-control" id="penerbit" name="penerbit" value="<?php if (isset($_POST["submit"])) echo $_POST["penerbit"]; ?>">
+            <?php if(isset($error["penerbit"])) {?>
+            <small id="judul" class="form-text text-danger"><?php echo $error["penerbit"];?></small> <?php } ?>
         </div>
         <div class="form-group mb-4">
             <div>
                 <label for="cover">Cover :</label></div>
             <input type="file" id="cover" name="cover">
+            <?php if(isset($error["cover"])) {?>
+            <small id="judul" class="form-text text-danger"><?php echo $error["cover"];?></small> <?php } ?>
         </div>
-        <button type="submit" class="btn btn-primary" name="tambah">Tambah Data</button>
+        <button type="submit" class="btn btn-primary" name="submit">Tambah Data</button>
     </form>
 </div>
 
@@ -58,5 +91,5 @@ require('template/header.php');
 
 
 <?php
-require('template/footer.php')
+require('template/footer.php');
 ?>
